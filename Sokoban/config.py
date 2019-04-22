b11b3d3be00600f3467081c68f22df034fe642b8
Gabriel <214524224@qq.com>
2019-04-22 21:07:02 +0800

    upload

---
+++ Sokoban/config.py
@@ -0,0 +1,11 @@
+Config = {
+			'WIDTH': 500,
+			'HEIGHT': 500,
+			'block_size': 50,
+			'levels_path': './levels',
+			'resources_path': './resources',
+			'imgfolder': 'imgs',
+			'fontfolder': 'font',
+			'audiofolder': 'audios',
+			'bg_color': (45, 45, 45)
+		}
